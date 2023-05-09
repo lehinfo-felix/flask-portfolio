7f3158b1983df33553a9e7da2d38fba96f2c653b
Leandro Felix <lehinfo.felix@gmail.com>
2023-05-09 18:59:48 -0300

    refactor html modules

--- routes/routes.py
+++ routes/routes.py
@@ -5,24 +5,29 @@ routes_bp = Blueprint("routes", __name__)
 
 @routes_bp.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", \
+    titulo_pagina="Início")
 
 
 @routes_bp.route("/sobre")
 def sobre():
-    return render_template("sobre.html")
+    return render_template("sobre.html", \
+    titulo_pagina="Sobre")
 
 
 @routes_bp.route("/experiencia")
 def experiencia():
-    return render_template("experiencia.html")
+    return render_template("experiencia.html", \
+    titulo_pagina="Sobre")
 
 
 @routes_bp.route("/formacao")
 def formacao():
-    return render_template("formacao.html")
+    return render_template("formacao.html", \
+    titulo_pagina="Sobre")
 
 
 @routes_bp.route("/contato")
 def contato():
-    return render_template("contato.html")
+    return render_template("contato.html", \
+    titulo_pagina="Sobre")
